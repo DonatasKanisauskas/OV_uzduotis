b0e22e1125b27c09f405bf5c1984853eebae22c9
Donatas <doweris@gmail.com>
2021-07-24 19:59:48 +0300

    added aditional pages

---
+++ blog.php
@@ -0,0 +1,15 @@
+<?php include("templates/header.html") ?>
+
+<div class="main-container">
+  <div class="left-panel">
+    <div class="main-block rot36-min reverse">
+      <div class="rot36-plus reverse"></div>
+    </div>
+  </div>
+</div>
+<div class="side-container">
+  <?php include('navbar.php'); ?>
+  <div class="content-box"></div>
+</div>
+
+<?php include("templates/footer.html") ?>
