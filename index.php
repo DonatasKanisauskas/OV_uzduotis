3f601d5323e4882351a46cb3dd1201ac5362c82a
Donatas <doweris@gmail.com>
2021-07-24 19:53:20 +0300

    refactored code from html to php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include("templates/header.html") ?>
+
+<div class="main-container">
+  <div class="bg-cover">
+    <div class="background-cover"></div>
+  </div>
+  <div class="left-panel">
+    <div class="main-block rot36-min">
+      <div class="rot36-plus"></div>
+    </div>
+  </div>
+  <div class="right-panel">
+    <a href="resume.php">
+      <div id="b1" class="block rot36-min">
+        <div class="resume rot36-plus">
+          <h2>RESUME</h2>
+        </div>
+      </div>
+    </a>
+    <a href="portfolio.php">
+      <div id="b2" class="block rot36-min">
+        <div class="portfolio rot36-plus">
+          <h2>PORTFOLIO</h2>
+        </div>
+      </div>
+    </a>
+    <a href="blog.php">
+      <div id="b3" class="block rot36-min">
+        <div class="blog rot36-plus">
+          <h2>BLOG</h2>
+        </div>
+      </div>
+    </a>
+    <a href="contact.php">
+      <div id="b4" class="block rot36-min">
+        <div class="contact rot36-plus">
+          <h2>CONTACT</h2>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+<div class="content"></div>
+
+<?php include("templates/footer.html") ?>
