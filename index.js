fc9d479750f82848e2d46cb57c8c0cb079b1ce4b
Donatas <doweris@gmail.com>
2021-07-28 21:00:19 +0300

    cursor with smooth collowing animation

--- index.js
+++ index.js
@@ -13,10 +13,25 @@ $("a").hover(
   }
 );
 
-$("body").mousemove(function () {
+let mouseX = (mouseY = ballX = ballY = 0);
+let speed = 0.1;
+
+function animate() {
+  console.log("asd");
+  let distX = mouseX - ballX;
+  let distY = mouseY - ballY;
+  ballX += distX * speed;
+  ballY += distY * speed;
   $(".cursor").css({
-    top: event.clientY - 10 + "px",
-    left: event.clientX - 10 + "px",
+    left: ballX - 10 + "px",
+    top: ballY - 10 + "px",
     display: "block",
   });
+  requestAnimationFrame(animate);
+}
+animate();
+
+$("body").mousemove(function (event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
 });
